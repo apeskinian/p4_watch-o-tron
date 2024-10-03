b4c572f5a3f954f0e2bec4c4bbd26e244085ad25
Matthew Aitken <matt@itken.net>
2024-10-03 13:36:38 +0000

    Set DEBUG to True for Heroku test

--- watchotron/settings.py
+++ watchotron/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 development = os.environ.get('DEVELOPMENT', False)
-DEBUG = development
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.ws.codeinstitute-ide.net',
