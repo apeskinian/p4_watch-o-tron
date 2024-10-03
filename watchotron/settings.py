a25fb4d1810f7524c9a9032d0bf919b9dc1202f0
Matthew Aitken <matt@itken.net>
2024-10-03 14:16:19 +0000

    Change DEBUG back to auto set

--- watchotron/settings.py
+++ watchotron/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 development = os.environ.get('DEVELOPMENT', False)
-DEBUG = True
+DEBUG = development
 
 ALLOWED_HOSTS = [
     '.ws.codeinstitute-ide.net',
