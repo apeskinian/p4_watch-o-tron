ce5e1083d3a86af872a4c34eae445f402562762f
Matthew Aitken <matt@itken.net>
2024-10-04 10:29:08 +0000

    Add list, movement and watch models to database

--- watches/admin.py
+++ watches/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import WatchList, WatchMovement, Watch
 
-# Register your models here.
+@admin.register(WatchList)
+class WatchListAdmin(admin.ModelAdmin):
+    list_display = ('list_name',)
+
+@admin.register(WatchMovement)
+class WatchMovementAdmin(admin.ModelAdmin):
+    list_display = ('movement_name',)
+
+@admin.register(Watch)
+class WatchAdmin(admin.ModelAdmin):
+    list_display = ('make', 'collection', 'model', 'owner')
+    list_filter = ('owner', 'list_name')
--- watches/models.py
+++ watches/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class WatchList(models.Model):
+    list_name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.list_name
+
+class WatchMovement(models.Model):
+    movement_name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.movement_name
+
+class Watch(models.Model):
+    # general watch details
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watch_owner')
+    list_name = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='watch_list')
+    movement_type = models.ForeignKey(WatchMovement, on_delete=models.CASCADE, related_name='watch_movement')
+    make = models.CharField(max_length=100, null=False, blank=False)
+    collection = models.CharField(max_length=100, blank=True)
+    model = models.CharField(max_length=100, blank=True)
+    movement_model = models.CharField(max_length=50, blank=True)
+    image = CloudinaryField('image', default='placeholder', asset_folder='/wot_watches/')
+    # complications
+    complication_chronograph = models.BooleanField(default=False)
+    complication_date = models.BooleanField(default=False)
+    complication_day = models.BooleanField(default=False)
+    complication_gmt = models.BooleanField(default=False)
+    complication_world_timer = models.BooleanField(default=False)
+    complication_moonphase = models.BooleanField(default=False)
+    complication_power_reserve = models.BooleanField(default=False)
+    complication_tourbillon = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.make} {self.collection} {self.model}'
