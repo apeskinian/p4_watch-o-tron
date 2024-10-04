e458fa950cbe862ff1c09142a55d7d577a4aaaf8
Matthew Aitken <matt@itken.net>
2024-10-04 08:39:19 +0000

    Add base.html and link up home.html template

--- watches/views.py
+++ watches/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-def index(request):
-    return HttpResponse('Testing')
+def home(request):
+    return render(request, 'watches/home.html')
