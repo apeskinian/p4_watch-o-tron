1ed864626fbc6ec0f3b6ff8d86137e853b7724af
Matthew Aitken <matt@itken.net>
2024-10-04 11:27:56 +0000

    Show logged in users their all their watches

--- watches/views.py
+++ watches/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Watch, WatchList
 
 def home(request):
     if request.user.is_authenticated:
-        return render(request, 'watches/home.html')
+        watches = Watch.objects.filter(owner=request.user)
+        context = {
+            'watches': watches
+        }
+        return render(request, 'watches/home.html', context)
     else:
         return render(request, 'account/login.html')
     
