3f28c771b517542cfd178dfe86f86d0d75338408
Matthew Aitken <matt@itken.net>
2024-10-04 09:17:00 +0000

    Add user creation, login and logout

--- watches/views.py
+++ watches/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    return render(request, 'watches/home.html')
+    if request.user.is_authenticated:
+        return render(request, 'watches/home.html')
+    else:
+        return render(request, 'account/login.html')
+    
