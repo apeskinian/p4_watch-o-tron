68acf9acab24d415102f46005b1955dc354bffba
Matthew Aitken <matt@itken.net>
2024-10-07 08:36:52 +0000

    Add URL pattern to account for new lists

--- watches/urls.py
+++ watches/urls.py
@@ -4,4 +4,5 @@ from . import views
 # working code
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<str:list_name>', views.home, name='watch_list')
 ]
--- watches/views.py
+++ watches/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Watch, WatchList
 
 
-# working code
-def home(request):
+def home(request, list_name='Collection'):
     if request.user.is_authenticated:
         watches = Watch.objects.filter(owner=request.user)
-        lists = WatchList.objects.all()
+        lists = WatchList.objects.values_list('list_name', flat=True)
+        current_list = list_name
         context = {
             'watches': watches,
-            'lists': lists
+            'lists': lists,
+            'current_list': current_list
         }
         return render(request, 'watches/home.html', context)
     else:
