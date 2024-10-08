3ad33e27f6edce8034ccb735360ef6fc61a29a87
Matthew Aitken <matt@itken.net>
2024-10-08 09:58:47 +0000

    Change login check to @login_required for test

--- watches/views.py
+++ watches/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Watch, WatchList
 from .forms import WatchForm
 
-
+@login_required(login_url='accounts/login')
 def home(request, list_name='Collection'):
-    if request.user.is_authenticated:
-        watches = Watch.objects.filter(
-            owner=request.user,
-            list_name__list_name=list_name
-        )
-        lists = WatchList.objects.values_list('list_name', flat=True)
-        current_list = list_name
-        context = {
-            'watches': watches,
-            'lists': lists,
-            'current_list': current_list
-        }
-        return render(request, 'watches/home.html', context)
-    else:
-        return render(request, 'account/login.html')
+    watches = Watch.objects.filter(
+        owner=request.user,
+        list_name__list_name=list_name
+    )
+    lists = WatchList.objects.values_list('list_name', flat=True)
+    current_list = list_name
+    context = {
+        'watches': watches,
+        'lists': lists,
+        'current_list': current_list
+    }
+    return render(request, 'watches/home.html', context)
+
 
 def add_watch(request):
     print('add_watch starting')
