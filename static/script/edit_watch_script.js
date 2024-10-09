5fb0ec3b8a9bad1bbf0e9a7a792ffd5d254b83c4
Matthew Aitken <matt@itken.net>
2024-10-09 12:52:42 +0000

    Add basic confirmation for watch addition

--- static/script/edit_watch_script.js
+++ static/script/edit_watch_script.js
@@ -1,13 +1,33 @@
 // EDITING
+// get form
+const watchForm = document.getElementById('watch-form')
 // get button
 const editButton = document.getElementById('btn-edit');
 // getting modal elements for deleting and purchasing
 const watchModal = new bootstrap.Modal(document.getElementById('watch-modal'));
 const watchModalTitle = document.getElementById('watch-modal-title');
 const watchModalBody = document.getElementById('watch-modal-body');
+const watchModalConfirm = document.getElementById('watch-modal-confirm');
+
+// Function to validate form
+function validateForm() {
+    // Check each required field for validity
+    return watchForm.checkValidity();
+}
 
 editButton.addEventListener('click', (e) => {
-    console.log('button was clicked now what?')
-    watchModalTitle.innerText = 'Add watch?';
-    watchModal.show();
+    if (validateForm()) {
+        // If valid, populate modal and show it
+        watchModalTitle.innerText = 'Add watch?';
+        watchModal.show();
+    } else {
+        // If invalid, trigger native form validation
+        watchForm.reportValidity();
+    }
+});
+
+// Handle modal confirm button click
+watchModalConfirm.addEventListener('click', function () {
+    // Once confirmed, submit the form
+    watchForm.submit();
 });
