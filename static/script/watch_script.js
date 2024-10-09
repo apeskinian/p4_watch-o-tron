7a800e86834cee378d42ef0b425d677d1793f25d
Matthew Aitken <matt@itken.net>
2024-10-09 10:26:55 +0000

    Add purchase confirmation for watches

--- static/script/watch_script.js
+++ static/script/watch_script.js
@@ -1,8 +1,12 @@
-const deleteModal = new bootstrap.Modal(document.getElementById('delete-watch-modal'));
+
 const deleteButtons = document.getElementsByClassName('btn-delete');
-const deleteConfirm = document.getElementById('delete-watch-confirm');
-const deleteMessage = document.getElementById('delete-watch-message');
-const deleteTitle = document.getElementById('delete-watch-title');
+const purchasedButtons = document.getElementsByClassName('btn-purchased');
+
+const deleteModal = new bootstrap.Modal(document.getElementById('watch-modal'));
+const deleteTitle = document.getElementById('watch-modal-title');
+const deleteMessage = document.getElementById('watch-modal-message');
+const deleteConfirm = document.getElementById('watch-modal-confirm');
+
 
 for (let button of deleteButtons) {
     button.addEventListener('click', (e) => {
@@ -12,6 +16,21 @@ for (let button of deleteButtons) {
         deleteTitle.innerText = `Delete ${watchName}?`
         deleteMessage.innerText = `This will delete this watch from you ${watchList}.`
         deleteConfirm.href = `/delete/${watchId}`;
+        deleteConfirm.innerText = 'Delete';
+        deleteConfirm.classList.add('btn-danger');
+        deleteModal.show();
+    });
+}
+
+for (let button of purchasedButtons) {
+    button.addEventListener('click', (e) => {
+        let watchId = e.target.getAttribute('data-watch_id');
+        let watchName = e.target.getAttribute('data-watch_name');
+        deleteTitle.innerText = `Confirm purchase of ${watchName}?`
+        deleteMessage.innerText = `This will move this watch to your collection.`
+        deleteConfirm.href = `/purchase/${watchId}`;
+        deleteConfirm.innerText = 'Confirm';
+        deleteConfirm.classList.add('btn-success');
         deleteModal.show();
     });
 }
