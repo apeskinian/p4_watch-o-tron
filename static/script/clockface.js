ce589357e1512487436f1ff84f00e7b470b4009e
Matthew Aitken <matt@itken.net>
2024-10-18 09:24:38 +0000

    Change main logo to include clockface.html

--- static/script/clockface.js
+++ static/script/clockface.js
@@ -1,4 +1,4 @@
-var inc = 1000;
+var inc = 60000;
 
 clock();
 
@@ -7,15 +7,12 @@ function clock() {
 
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
   
   const hour = hours * 30;
   const minute = minutes * 6;
-  const second = seconds * 6;
   
   document.querySelector('.hour').style.transform = `rotate(${hour}deg)`
   document.querySelector('.minute').style.transform = `rotate(${minute}deg)`
-  document.querySelector('.second').style.transform = `rotate(${second}deg)`
 }
 
 setInterval(clock, inc);
