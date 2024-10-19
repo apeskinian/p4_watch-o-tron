4b066ee6f6c79ffa6c6da3e6df82148e190d6663
Matthew Aitken <matt@itken.net>
2024-10-19 09:01:25 +0000

    Add tooltips to complication icons

--- static/script/base_script.js
+++ static/script/base_script.js
@@ -1,6 +1,11 @@
+// initialise tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 // set timeout of messages
 setTimeout(function() {
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 3000);
+
