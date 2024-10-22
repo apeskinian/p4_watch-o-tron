782810f37cb4a04af41d7c093cffedec76d8ce51
Matthew Aitken <matt@itken.net>
2024-10-22 09:06:58 +0000

    Style watch confirm modal for add/edit

--- static/script/edit_watch_script.js
+++ static/script/edit_watch_script.js
@@ -3,6 +3,11 @@
 const watchForm = document.getElementById('watch-form')
 // get button
 const editButton = document.getElementById('btn-edit');
+// get card elements
+const watchModalMake = document.getElementById('watch-modal-make')
+const watchModalCollectionModel = document.getElementById('watch-modal-collection-model')
+const watchModalWatchMovement = document.getElementById('watch-modal-movement')
+const watchModalList = document.getElementById('watch-modal-list')
 // get complication icons
 const compChronograph = document.getElementById('complication-chronograph')
 const compDate = document.getElementById('complication-date')
@@ -14,21 +19,11 @@ const compPowerReserve = document.getElementById('complication-power-reserve')
 const compTourbillon = document.getElementById('complication-tourbillon')
 // getting modal elements for deleting and purchasing
 const watchModal = new bootstrap.Modal(document.getElementById('watch-modal'));
-const watchModalTitle = document.getElementById('watch-modal-title');
-const watchModalBody = document.getElementById('watch-modal-body');
 const watchModalConfirm = document.getElementById('watch-modal-confirm');
 
 // validating form
 function validateForm() {
     return watchForm.checkValidity();
-}
-
-function getComplications(comp) {
-    if (watchForm.elements[comp].checked) {
-        return ''
-    } else {
-        return 'not-complicated'
-    }
 }
 
 editButton.addEventListener('click', (e) => {
@@ -46,26 +41,42 @@ editButton.addEventListener('click', (e) => {
         }
 
         var selectedMovement = watchForm.elements['movement_type'];
-        var selectedMovementText = selectedMovement.options[selectedMovement.selectedIndex].text;
         var selectedList = watchForm.elements['list_name'];
-        var selectedListText = selectedList.options[selectedList.selectedIndex].text;
-        watchModalTitle.innerText = 'Confirm details:';
-        watchModalBody.innerHTML = `
-            <p><strong>Make:</strong> ${watchForm.elements['make'].value}</p>
-            <p><strong>Collection:</strong> ${watchForm.elements['collection'].value}</p>
-            <p><strong>Model:</strong> ${watchForm.elements['model'].value}</p>
-            <p><strong>Movement Type:</strong> ${selectedMovementText}</p>
-            <p><strong>Movement Model:</strong> ${watchForm.elements['movement_model'].value}</p>
-            <p><strong>Complication Chronograph:</strong> ${watchForm.elements['complication_chronograph'].checked}</p>
-            <p><strong>Complication Date:</strong> ${watchForm.elements['complication_date'].checked}</p>
-            <p><strong>Complication Day:</strong> ${watchForm.elements['complication_day'].checked}</p>
-            <p><strong>Complication GMT:</strong> ${watchForm.elements['complication_gmt'].checked}</p>
-            <p><strong>Complication World Timer:</strong> ${watchForm.elements['complication_world_timer'].checked}</p>
-            <p><strong>Complication Moonphase:</strong> ${watchForm.elements['complication_moonphase'].checked}</p>
-            <p><strong>Complication Power Reserve:</strong> ${watchForm.elements['complication_power_reserve'].checked}</p>
-            <p><strong>Complication Tourbillon:</strong> ${watchForm.elements['complication_tourbillon'].checked}</p>
-            <p><strong>List:</strong> ${selectedListText}</p>
-        `;
+
+        watchModalMake.innerText = watchForm.elements['make'].value;
+        watchModalCollectionModel.innerText = watchForm.elements['collection'].value;
+        if (watchForm.elements['model'].value != '') {
+            watchModalCollectionModel.innerText += ` ${watchForm.elements['model'].value}`
+        }
+        watchModalWatchMovement.innerText = selectedMovement.options[selectedMovement.selectedIndex].text;
+        if (watchForm.elements['movement_model'].value != '') {
+            watchModalWatchMovement.innerText += ` (${watchForm.elements['movement_model'].value})`
+        }
+        watchModalList.innerText = selectedList.options[selectedList.selectedIndex].text;
+        if (watchForm.elements['complication_chronograph'].checked) {
+            compChronograph.classList.remove('not-complicated')
+        }
+        if (watchForm.elements['complication_date'].checked) {
+            compDate.classList.remove('not-complicated')
+        }
+        if (watchForm.elements['complication_day'].checked) {
+            compDay.classList.remove('not-complicated')
+        }
+        if (watchForm.elements['complication_gmt'].checked) {
+            compGmt.classList.remove('not-complicated')
+        }
+        if (watchForm.elements['complication_world_timer'].checked) {
+            compWorldTimer.classList.remove('not-complicated')
+        }
+        if (watchForm.elements['complication_moonphase'].checked) {
+            compMoonphase.classList.remove('not-complicated')
+        }
+        if (watchForm.elements['complication_power_reserve'].checked) {
+            compPowerReserve.classList.remove('not-complicated')
+        }
+        if (watchForm.elements['complication_tourbillon'].checked) {
+            compTourbillon.classList.remove('not-complicated')
+        }
         watchModal.show();
     } else {
         watchForm.reportValidity();
