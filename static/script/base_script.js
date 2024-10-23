f3fb22f4091afe301fc091d9967502df363677c2
Matthew Aitken <matt@itken.net>
2024-10-23 10:33:27 +0000

    Stop error messages from auto closing

--- static/script/base_script.js
+++ static/script/base_script.js
@@ -3,11 +3,13 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 // set timeout of messages
-// setTimeout(function() {
-//     let messages = document.querySelectorAll('#msg');
-//     messages.forEach(function(message) {
-//         let alert = new bootstrap.Alert(message);
-//         alert.close();
-//     });
-// }, 3000);
+setTimeout(function() {
+    let messages = document.querySelectorAll('#msg');
+    messages.forEach(function(message) {
+        if (!message,classList.contains('alert-danger')) {
+            let alert = new bootstrap.Alert(message);
+            alert.close();
+        }
+    });
+}, 3000);
 
