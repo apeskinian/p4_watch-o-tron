29841539057558436fefe90f601ed0d1caa7a5a9
Matthew Aitken <matt@itken.net>
2024-10-29 11:07:15 +0000

    Set debug true for test

--- watchotron/settings.py
+++ watchotron/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 development = os.environ.get('DEVELOPMENT', False)
-DEBUG = development
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
