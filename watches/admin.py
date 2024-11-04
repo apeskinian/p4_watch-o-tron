9d2ad1fca68f70203bd4f95f7cd31af71ea548c7
Matthew Aitken <matt@itken.net>
2024-11-04 11:36:55 +0000

    Amend admin.py for PEP8 validation

--- watches/admin.py
+++ watches/admin.py
@@ -4,18 +4,22 @@ from allauth.account.models import EmailAddress
 from django.contrib.sites.models import Site
 from .models import WatchList, WatchMovement, Watch
 
+
 @admin.register(WatchList)
 class WatchListAdmin(admin.ModelAdmin):
     list_display = ('friendly_name',)
+
 
 @admin.register(WatchMovement)
 class WatchMovementAdmin(admin.ModelAdmin):
     list_display = ('movement_name',)
 
+
 @admin.register(Watch)
 class WatchAdmin(admin.ModelAdmin):
     list_display = ('make', 'collection', 'model', 'owner')
     list_filter = ('owner', 'list_name')
+
 
 admin.site.unregister(SocialToken)
 admin.site.unregister(SocialAccount)
