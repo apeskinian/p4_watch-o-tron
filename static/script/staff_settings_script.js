386c85a5b09005638b779f536f2be4020d67aefa
Matthew Aitken <matt@itken.net>
2024-11-08 09:10:09 +0000

    Add spinners to staff settings buttons

--- static/script/staff_settings_script.js
+++ static/script/staff_settings_script.js
@@ -9,3 +9,18 @@ window.onload = () => {
         settingsModal.show();
     }
   };
+
+const addMovementButton = document.getElementById('add-movement-btn');
+const addListButton = document.getElementById('add-list-btn');
+const modalConfirmButton = document.getElementById('modal-confirm');
+const modalCancelButton = document.getElementById('modal-cancel');
+
+let allBtns = [addMovementButton, addListButton, modalConfirmButton, modalCancelButton];
+
+allBtns.forEach(function(button) {
+    button.addEventListener('click', function () {
+        button.innerHTML = `
+            <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+            <span role="status">Working...</span>`;
+    });
+})
